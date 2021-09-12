95b8bd7f949088d7d9d7716f8c9bbc01341cf65a
sigmaSd <bedisnbiba@gmail.com>
2021-09-12 11:43:38 +0000

    ifconfig is not required anymore

--- src/eltrafico_main/main.rs
+++ src/eltrafico_main/main.rs
@@ -7,7 +7,7 @@ use utils::check_for_dependencies;
 mod netmonitor;
 
 pub type CatchAll<T> = Result<T, Box<dyn std::error::Error>>;
-const DEPENDENCIES: [&str; 4] = ["tc", "ss", "ifconfig", "ip"];
+const DEPENDENCIES: [&str; 3] = ["tc", "ss", "ip"];
 
 fn main() {
     if let Err(e) = check_for_dependencies(&DEPENDENCIES) {
